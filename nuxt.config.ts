94e0c81dec06fe96bf5bde9dffe9bf26fcb1d0d9
claussimar <dasunheimliche7@gmail.com>
2024-04-07 02:10:52 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/tailwindcss"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="text-red-600 flex flex-col justify-center items-center h-full w-full"
+  >
+    <div>{{ contador }}</div>
+    <div class="flex gap-3">
+      <button @click="restar">restar</button>
+      <button @click="sumar">sumar</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+let contador = ref<number>(0);
+
+function sumar() {
+  contador.value++;
+}
+
+function restar() {
+  contador.value--;
+}
+</script>
